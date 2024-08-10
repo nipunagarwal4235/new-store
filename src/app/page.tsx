d1a49096f4e8d17e7fc96e9031af23eac856e786
Nipun Agarwal <nipunagarwal322@gmail.com>
2024-08-10 13:54:41 +0530

    feat: added a simple navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import Header from '@/components/Header';
 import Image from 'next/image';
 
 export default function Home() {
   return (
     <div>
-      <h1>Hello</h1>
+      <Header />
     </div>
   );
 }
